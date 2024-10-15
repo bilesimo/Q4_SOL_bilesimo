5f1e15828a7ac07e9a2da734e2106c5d415afdbf
bilesimo <stang3tzzz@gmail.com>
2024-10-15 19:26:26 -0300

    Add logic to transfer to some tokens to another wallet

--- ts/cluster1/spl_transfer.ts
+++ ts/cluster1/spl_transfer.ts
@@ -1,5 +1,11 @@
-import { Commitment, Connection, Keypair, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js"
-import wallet from "../wba-wallet.json"
+import {
+  Commitment,
+  Connection,
+  Keypair,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+} from "@solana/web3.js";
+import wallet from "../wba-wallet.json";
 import { getOrCreateAssociatedTokenAccount, transfer } from "@solana/spl-token";
 
 // We're going to import our keypair from the wallet file
@@ -10,19 +16,39 @@ const commitment: Commitment = "confirmed";
 const connection = new Connection("https://api.devnet.solana.com", commitment);
 
 // Mint address
-const mint = new PublicKey("<mint address>");
+const mint = new PublicKey("2vgRMzuHPgjaRn19Xj8udXY2qSze9YQW9pJpZc5rC5vA");
 
 // Recipient address
-const to = new PublicKey("<receiver address>");
+const to = new PublicKey("GYFZiNkQADCPS6u2gvMZLFgQ8jJc6a1ySWxfHdMsLvTD");
 
 (async () => {
-    try {
-        // Get the token account of the fromWallet address, and if it does not exist, create it
+  try {
+    // Get the token account of the fromWallet address, and if it does not exist, create it
+    const ataFrom = await getOrCreateAssociatedTokenAccount(
+      connection,
+      keypair,
+      mint,
+      keypair.publicKey
+    );
+    // Get the token account of the toWallet address, and if it does not exist, create it
+    const ataTo = await getOrCreateAssociatedTokenAccount(
+      connection,
+      keypair,
+      mint,
+      to
+    );
+    // Transfer the new token to the "toTokenAccount" we just created
+    const tx = await transfer(
+      connection,
+      keypair,
+      ataFrom.address,
+      ataTo.address,
+      keypair.publicKey,
+      1e6
+    );
 
-        // Get the token account of the toWallet address, and if it does not exist, create it
-
-        // Transfer the new token to the "toTokenAccount" we just created
-    } catch(e) {
-        console.error(`Oops, something went wrong: ${e}`)
-    }
+    console.log(tx);
+  } catch (e) {
+    console.error(`Oops, something went wrong: ${e}`);
+  }
 })();
