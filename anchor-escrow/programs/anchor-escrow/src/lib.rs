320fa7fed3086a1387379e8649b0b0b714bc029f
bilesimo <stang3tzzz@gmail.com>
2024-10-23 11:39:39 -0300

    Add anchor-escrow project with deposit and refund instructions

---
+++ anchor-escrow/programs/anchor-escrow/src/lib.rs
@@ -0,0 +1,29 @@
+use anchor_lang::prelude::*;
+
+declare_id!("Bopnz1jjngvNhxhMcCbHoqETrn7yayP1ru7MfFDLYSxJ");
+
+pub mod state;
+pub use state::*;
+
+pub mod instructions;
+pub use instructions::*;
+
+#[program]
+pub mod anchor_escrow {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Make>, seed: u64, deposit: u64, receive: u64) -> Result<()> {
+        ctx.accounts.init_escrow(seed, receive, &ctx.bumps)?;
+        ctx.accounts.deposit(deposit)?;
+
+        Ok(())
+    }
+
+    pub fn refund_and_close_vault(ctx: Context<Refund>) -> Result<()> {
+        ctx.accounts.refund_and_close_vault()?;
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
