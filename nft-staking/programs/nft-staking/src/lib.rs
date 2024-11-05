30d16de8da2069374d917bb6c0642efffef781e3
bilesimo <stang3tzzz@gmail.com>
2024-11-04 23:00:29 -0300

    Add nft-staking project

---
+++ nft-staking/programs/nft-staking/src/lib.rs
@@ -0,0 +1,42 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("3YNM4mZAeWBEAYrZzrRuovkWGurCDMw8dTUPr5b3nvSE");
+
+#[program]
+pub mod nft_staking {
+    use super::*;
+
+    pub fn initialize_user(ctx: Context<Initialize>) -> Result<()> {
+        ctx.accounts.initialize_user_account(&ctx.bumps)
+    }
+
+    pub fn initialize_config(
+        ctx: Context<InitializeConfig>,
+        points_per_stake: u8,
+        max_stake: u8,
+        freeze_period: u32
+    ) -> Result<()> {
+        ctx.accounts.initialize_config(points_per_stake, max_stake, freeze_period, &ctx.bumps)
+    }
+
+    pub fn stake(ctx: Context<Stake>) -> Result<()> {
+        ctx.accounts.stake(&ctx.bumps)
+    }
+
+    pub fn unstake(ctx: Context<Unstake>) -> Result<()> {
+        ctx.accounts.unstake()
+    }
+
+    pub fn claim(ctx: Context<Claim>) -> Result<()> {
+        ctx.accounts.claim()
+    }
+}
